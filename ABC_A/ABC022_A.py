662acf7a00a33ca125c0a621efe4c81949dff02e
ryosuke0825 <ayakobon@gmail.com>
2020-02-04 00:47:43 +0900

    Update ABC022_A.py
    AtCoder Beginner Contest 021 A問題 修正
    1回目の体重判定を追加

--- ABC_A/ABC022_A.py
+++ ABC_A/ABC022_A.py
@@ -1,9 +1,12 @@
 n, s, t = map(int, input().split())
 w = int(input())
-
 ret = 0
+
+if s <= w <= t:
+    ret += 1
+
 for _ in range(n-1):
-    w = w + int(input())
+    w += int(input())
     if s <= w <= t:
         ret += 1
 print(ret)
