05da95ef3b39e5d84423cd3392b462f41a95801d
ryosuke0825 <ayakobon@gmail.com>
2020-02-19 13:25:18 +0900

    Create AtCoder ABC 004 B
    AtCoder Beginner Contest 004 B問題 作成

---
+++ ABC_B/ABC004_B.py
@@ -0,0 +1,9 @@
+c0 = input().split()
+c1 = input().split()
+c2 = input().split()
+c3 = input().split()
+
+print(" ".join(c3[::-1]))
+print(" ".join(c2[::-1]))
+print(" ".join(c1[::-1]))
+print(" ".join(c0[::-1]))
