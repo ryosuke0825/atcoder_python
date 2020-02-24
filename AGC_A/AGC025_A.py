f5aab572e44d3c4f0d3c6c9c2c3f0a363e2680b9
ryosuke0825 <ayakobon@gmail.com>
2020-02-25 01:40:35 +0900

    Update AtCoder AGC 025 A
    AtCoder Grand Contest 025 A問題 修正

--- AGC_A/AGC025_A.py
+++ AGC_A/AGC025_A.py
@@ -1,17 +1,6 @@
-n = int(input())
-if n%10==0:
+n = list(map(int, list(input())))
+
+if n[0] == 1 and sum(n) == 1:
     print(10)
-    exit(0)
-ans = pow(10,10)
-for a in range(n+1):
-    b = n-a
-    tmp_ans = 0
-
-    for sa in str(a):
-        tmp_ans += int(sa)
-
-    for sb in str(b):
-        tmp_ans += int(sb)
-
-    ans = min(ans, tmp_ans)
-print(ans)
+else:
+    print(sum(n))
