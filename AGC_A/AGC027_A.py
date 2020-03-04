6a57f4a1eaa60c4365b9fb4d5d5637524a28063c
ryosuke0825 <ayakobon@gmail.com>
2020-03-04 21:59:51 +0900

    Create AtCoder AGC 027 A
    AtCoder Grand Contest 027 A問題 作成

---
+++ AGC_A/AGC027_A.py
@@ -0,0 +1,18 @@
+N, x = map(int, input().split())
+a = list(map(int, input().split()))
+
+a.sort()
+ans = 0
+for i in range(N):
+    if i == N-1:
+        if a[i] == x:
+            x -= a[i]
+            ans += 1
+        else:
+            break
+    elif a[i] <= x:
+        x -= a[i]
+        ans += 1
+    else:
+        break
+print(ans)
