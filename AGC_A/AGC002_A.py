fff5835150e37b1956389c5a114c19b3cf0baa53
ryosuke0825 <ayakobon@gmail.com>
2020-03-04 21:58:41 +0900

    Create AtCoder ARC 002 A
    AtCoder Regular Contest 002 A問題 作成

---
+++ AGC_A/AGC002_A.py
@@ -0,0 +1,11 @@
+a, b = map(int, input().split())
+
+if a <= 0 <= b:
+    print('Zero')
+elif a > 0 and b > 0:
+    print('Positive')
+elif a < 0 and b < 0:
+    if (abs(a-b)+1) % 2 == 0:
+        print('Positive')
+    else:
+        print('Negative')
