abf37395f4e64893c5ccd9ea703d51c21f5b6037
ryosuke0825 <ayakobon@gmail.com>
2020-03-07 03:45:55 +0900

    Updata AtCoder AGC 041 A
    AtCoder Grand Contest 041 A問題 ファイル名修正

--- AGC_A/AGC041.py
+++ AGC_A/AGC041.py
@@ -3,6 +3,3 @@ if a % 2 == b % 2:
     print(abs(a-b)//2)
 else:
     print(min(a-1, n-b)+1+((b-a)//2))
-    # print(min(a-1, n-b))
-    # print(1)
-    # print(((b-a)//2))
--- AGC_A/AGC041_A.py
+++
@@ -1,6 +0,0 @@
-n, a, b = map(int, input().split())
-
-if abs(a-b) % 2 == 0:
-    print(abs(a-b)//2)
-else:
-    print(min((a-1), (b-1), abs(n-a), abs(n-b))+abs(a-b))
