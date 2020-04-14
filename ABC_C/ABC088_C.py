cd0eb1234163d1fa9c15cf14f19031e6f62d90c1
ryosuke0825 <ayakobon@gmail.com>
2020-04-14 21:21:03 +0900

    [update]AtCoder ABC 088 C
    自力AC

--- ABC_C/ABC088_C.py
+++ ABC_C/ABC088_C.py
@@ -1,7 +1,22 @@
-x, y = map(int, input().split())
+C = []
+for _ in range(3):
+    c1, c2, c3 = map(int, input().split())
+    tmp = [c1, c2, c3]
+    C.append(tmp)
 
-ans = 0
-while x <= y:
-    x *= 2
-    ans += 1
-print(ans)
+if not(C[0][0]-C[0][1] == C[1][0]-C[1][1] == C[2][0]-C[2][1]):
+    print('No')
+    exit(0)
+
+if not(C[0][1]-C[0][2] == C[1][1]-C[1][2] == C[2][1]-C[2][2]):
+    print('No')
+    exit(0)
+
+if not(C[0][0]-C[1][0] == C[0][1]-C[1][1] == C[0][2]-C[1][2]):
+    print('No')
+    exit(0)
+
+if not(C[1][0]-C[2][0] == C[1][1]-C[2][1] == C[1][2]-C[2][2]):
+    print('No')
+    exit(0)
+print('Yes')
