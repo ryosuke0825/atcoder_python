2c4f5ffae4b1d39a76eabe658c4f419ec451c29f
ryosuke0825 <ayakobon@gmail.com>
2020-04-15 22:34:25 +0900

    [add]AtCoder ABC 039 C
    自力AC

---
+++ ABC_C/ABC039_C.py
@@ -0,0 +1,16 @@
+S = input()
+
+if S == 'WBWBWWBWBWBWWBWBWWBW':
+    print('Do')
+elif S == 'WBWWBWBWBWWBWBWWBWBW':
+    print('Re')
+elif S == 'WWBWBWBWWBWBWWBWBWBW':
+    print('Mi')
+elif S == 'WBWBWBWWBWBWWBWBWBWW':
+    print('Fa')
+elif S == 'WBWBWWBWBWWBWBWBWWBW':
+    print('So')
+elif S == 'WBWWBWBWWBWBWBWWBWBW':
+    print('La')
+elif S == 'WWBWBWWBWBWBWWBWBWWB':
+    print('Si')
