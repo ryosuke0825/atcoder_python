17fd0c7abcce7d10be3126a7e8727d911a18636f
ryosuke0825 <ayakobon@gmail.com>
2020-05-17 20:41:12 +0900

    Update ABC150_C.py

--- ABC_C/ABC150_C.py
+++ ABC_C/ABC150_C.py
@@ -2,13 +2,14 @@ import itertools
 
 n = int(input())
 P = input().split()
+Q = input().split()
+p = ''.join(P)
+q = ''.join(Q)
+P.sort()
 numbers = list(itertools.permutations(P))
-P = ''.join(P)
-Q = input()
-Q = Q.replace(' ', '')
 
 numbers_str = []
 for num in numbers:
     numbers_str.append(''.join(num))
-numbers_str.sort()
-print(abs(numbers_str.index(P)-numbers_str.index(Q)))
+
+print(abs(numbers_str.index(p)-numbers_str.index(q)))
