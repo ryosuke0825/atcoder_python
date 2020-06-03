02ab376102b47818b407d97929c2af060904ce07
ryosuke0825 <ayakobon@gmail.com>
2020-06-04 07:57:42 +0900

    [update]AtCoder ABC 129 C
    TLEを解消

--- ABC_C/ABC129_C.py
+++ ABC_C/ABC129_C.py
@@ -1,5 +1,8 @@
 N, M = map(int, input().split())
-a = [int(input()) for i in range(M)]
+A = {}
+for _ in range(M):
+    a = int(input())
+    A[a] = True
 
 dp = [0]*(N+1)
 dp[0] = 1
@@ -9,9 +12,9 @@ for i in range(2, N+1):
     # 壊れた床がなく単純に求める場合
     #dp[i] = dp[i-1]+dp[i-2]
 
-    if not i-1 in a:
+    if not i-1 in A:
         dp[i] += dp[i-1]
-    if not i-2 in a:
+    if not i-2 in A:
         dp[i] += dp[i-2]
 
 print(dp[N] % 1000000007)
