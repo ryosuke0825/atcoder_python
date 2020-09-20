093a904d221dbd6211a7a97a6ae8b506dac6413d
ryosuke0825 <ayakobon@gmail.com>
2020-09-20 09:27:56 +0900

    [update]コメント付与

--- ABC_D/ABC179_D.py
+++ ABC_D/ABC179_D.py
@@ -11,16 +11,22 @@ dp = [0]*(N+1)
 dpsum = [0]*(N+1)
 dp[1] = 1
 dpsum[1] = 1
-for i in range(2, N+1):
-    for l, r in lr:
-        lj = max(1, i-r)
-        rj = i-l
 
-        if rj < 1:
+# 区間の合計を複数回求める＝累積和
+# 累積和を作りながらDPをする
+for i in range(2, N+1):
+    # マスiに移動できる数を求める
+    for l, r in lr:
+        # iの左側からiへ移動できる区間を求めるので、
+        # lとrは逆になる
+        li = max(i-r, 1)
+        ri = i-l
+
+        # 右のマスが0以下なら移動できないので次へ
+        if ri < 1:
             continue
 
-        dp[i] += dpsum[rj]-dpsum[lj-1]
+        dp[i] += dpsum[ri]-dpsum[li-1]
         dp[i] %= MOD
-    dpsum[i] = dp[i]+dpsum[i-1]
-
+    dpsum[i] = dpsum[i-1]+dp[i]
 print(dp[N])
